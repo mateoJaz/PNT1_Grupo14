454e25c24b66fb993f3b1c647f03c4bdb50b5dca
Mateo Jazan <40675828@comunidadort.edu.ar>
2025-09-10 21:52:23 -0300

    refinado de validaciones

--- TP3/Tp1/main.js
+++ TP3/Tp1/main.js
@@ -1,17 +1,37 @@
 const form = document.getElementById('formulario')
 form.addEventListener('submit', function(event) {
     event.preventDefault(); 
-    console.log('entre al submit')
     const fNombre = document.getElementById('fNombre');
     const fEmail = document.getElementById('fEmail');
     const fTelefono = document.getElementById('fTelefono');
     const fPassword = document.getElementById('fPassword');
+    let alerta = '';
 
-    if (fNombre.value === '' || fEmail.value === '' ||fTelefono.value === '' ||fPassword.value === ''){
-        alert("Por favor complete sus datos antes de cargar el formulario");
+    if (nombreInvalido(fNombre.value)) {alerta+= 'Su nombre no esta completo\n';}
+    if (emailInvalido(fEmail.value)) {alerta+= 'Su email no es valido\n';}
+    if (telefonoInvalido(fTelefono.value)) {alerta+= 'Su telefono no es valido\n';}
+    if (passwordInvalido(fPassword.value)) {alerta+= 'Su password no es valida\n';}
 
-    }else {
-        form.submit();
-
+    if(alerta === '') {
+        form.submit()
+    }else{
+        alert(alerta)
     }
+    
 });
+
+function nombreInvalido(nombre) {
+  return nombre === '';
+}
+function emailInvalido(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //email base, redundante?
+    return !emailRegex.test(email);
+}
+function telefonoInvalido(telefono) {
+    const telRegex = /^\d{8}$/ //8 digitos
+    return !telRegex.test(telefono);
+}
+function passwordInvalido(pass) {
+    const passRegex = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/; //1 mayuscula, 1 miniscula, 8 caracteres
+    return !passRegex.test(pass);
+}
