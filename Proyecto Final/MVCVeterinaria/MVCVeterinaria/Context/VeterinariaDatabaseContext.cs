8b7ca7aeb011f4008e7602c63137754d8b4a66c0
MarianoN17 <34690985@comunidadort.edu.ar>
2025-11-12 20:16:53 -0300

    Creacion del proyecto

---
+++ Proyecto Final/MVCVeterinaria/MVCVeterinaria/Context/VeterinariaDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCVeterinaria.Models;
+using System.Collections.Generic;
+
+namespace MVCVeterinaria.Context
+{
+    public class VeterinariaDatabaseContext : DbContext
+    {
+        public VeterinariaDatabaseContext(DbContextOptions<VeterinariaDatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
---
+++ Proyecto Final/MVCVeterinaria/MVCVeterinaria/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace MVCVeterinaria.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
